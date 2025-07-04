ebbcade9dedbca9ae429e45ef365cfab18277e91
glaudine <111119118+glaudinekorrie@users.noreply.github.com>
2025-07-04 17:10:37 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,5 +1,5 @@
-1. To transform the ProductDetail table into First Normal Form (1NF), each product should be in a separate row. Here's the SQL query to achieve that:
-
+Question 1 Achieving 1NF (First Normal Form:
+  
 CREATE TABLE ProductDetail_1NF (
   OrderID INT,
   CustomerName VARCHAR(100),
@@ -14,3 +14,4 @@ VALUES
 (102, 'Jane Smith', 'Keyboard'),
 (102, 'Jane Smith', 'Mouse'),
 (103, 'Emily Clark', 'Phone');
+
