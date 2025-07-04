010aebd36f34fdd34a0393262e0cc3e1617012ca
glaudine <111119118+glaudinekorrie@users.noreply.github.com>
2025-07-04 17:11:39 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -15,3 +15,36 @@ VALUES
 (102, 'Jane Smith', 'Mouse'),
 (103, 'Emily Clark', 'Phone');
 
+Question 2 Achieving 2NF (Second Normal Form):
+
+Step 1: Create Orders table (stores OrderID and CustomerName)
+sql
+CREATE TABLE Orders (
+  OrderID INT PRIMARY KEY,
+  CustomerName VARCHAR(100)
+);
+
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+
+Step 2: Create OrderDetails table (stores product and quantity per order)
+sql
+CREATE TABLE OrderDetails (
+  OrderID INT,
+  Product VARCHAR(50),
+  Quantity INT,
+  FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO OrderDetails (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
